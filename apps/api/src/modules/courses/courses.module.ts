7d68cea8f0f972500fadb5b866941e40932576ff
AltarCloud <altercloud007@gmail.com>
2026-06-16 23:29:14 +0100

    fix: add UserEntity to CoursesModule - AdminGuard dependency resolution

--- apps/api/src/modules/courses/courses.module.ts
+++ apps/api/src/modules/courses/courses.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CourseEntity, LessonEntity, EnrollmentEntity } from '../../database/entities';
+import { CourseEntity, LessonEntity, EnrollmentEntity, UserEntity } from '../../database/entities';
 import { CoursesController, AdminCoursesController } from './courses.controller';
 import { CoursesService } from './courses.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CourseEntity, LessonEntity, EnrollmentEntity])],
+  imports: [TypeOrmModule.forFeature([CourseEntity, LessonEntity, EnrollmentEntity, UserEntity])],
   controllers: [CoursesController, AdminCoursesController],
   providers: [CoursesService],
   exports: [CoursesService],
